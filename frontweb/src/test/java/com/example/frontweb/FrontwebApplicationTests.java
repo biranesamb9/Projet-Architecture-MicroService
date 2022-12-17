f50a1a3bc5b081387d1bf384d3955d205231fe3e
Birane SAMB <78302571+biranesamb9@users.noreply.github.com>
2022-12-17 17:22:22 +0100

    Les trois services : registry , microservice1 et frontweb

---
+++ frontweb/src/test/java/com/example/frontweb/FrontwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
